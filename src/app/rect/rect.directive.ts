842b591801dafa3343e1c3aa36c53c384ec93a9e
Thomas Heinrichs <thheinrichs@outlook.de>
2020-07-15 18:56:11 +0200

    Implement rect directive

--- src/app/rect/rect.directive.ts
+++ src/app/rect/rect.directive.ts
@@ -1,10 +1,87 @@
-import { Directive } from '@angular/core'
+import { Directive, Input, AfterViewChecked, OnDestroy, OnChanges, SimpleChanges } from '@angular/core'
+import { EventsDirective } from '../events/events.directive'
+import { Rect, Container } from '@svgdotjs/svg.js'
+import { ContainerService } from '../container-service/container.service'
 
 @Directive({
   selector: 'svg-rect'
 })
-export class RectDirective {
+export class RectDirective extends EventsDirective implements AfterViewChecked, OnDestroy, OnChanges {
 
-  constructor() { }
+  private _rect: Rect
+  private _container: Container
 
+  @Input() height: number | string
+  @Input() width: number | string
+  @Input() x: number | string = 0
+  @Input() y: number | string = 0
+  @Input() color: string = '#000'
+  @Input() classes: string[] = []
+  @Input() attrs: { [key: string]: any } = {}
+
+  constructor(
+    private _containerService: ContainerService
+  ) {
+    super()
+  }
+
+  ngAfterViewChecked() {
+    this._container = this._containerService.container
+    if (this._container && !this._rect) {
+      this._rect = this.create()
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (!this._rect) return
+
+    if (changes.width && changes.width.previousValue !== changes.width.currentValue) {
+      this._rect.width(changes.width.currentValue)
+    }
+
+    if (changes.height && changes.height.previousValue !== changes.height.currentValue) {
+      this._rect.height(changes.height.currentValue)
+    }
+
+    if (changes.x && changes.x.previousValue !== changes.x.currentValue) {
+      this._rect.x(changes.x.currentValue)
+    }
+
+    if (changes.y && changes.y.previousValue !== changes.y.currentValue) {
+      this._rect.x(changes.y.currentValue)
+    }
+
+    if (changes.width && changes.width.previousValue !== changes.width.currentValue) {
+      this._rect.width(changes.width.currentValue)
+    }
+
+    if (changes.color && changes.color.previousValue !== changes.color.currentValue) {
+      this._rect.fill(changes.color.currentValue)
+    }
+
+    if (changes.classes && changes.classes.previousValue !== changes.classes.currentValue) {
+      this._rect.attr('class', changes.classes.currentValue)
+    }
+
+    if (changes.attrs && changes.attrs.previousValue !== changes.attrs.currentValue) {
+      this._rect.attr(changes.attrs.currentValue)
+    }
+  }
+
+  ngOnDestroy() {
+    this._rect.remove()
+  }
+
+  create() {
+    const result = this._container
+      .rect()
+      .width(this.width)
+      .height(this.height)
+      .fill(this.color)
+      .move(this.x, this.y)
+      .attr(this.attrs)
+      .attr('class', this.classes)
+
+    return this.registerHandlers(result)
+  }
 }
